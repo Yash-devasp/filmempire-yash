b70eef6d5c3c8fbc5858ae2a1d9a596cc46e6fed
Yash Goswami <yashgoswami148@gmail.com>
2022-06-03 17:22:55 +0530

    search functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -18,7 +18,7 @@ import { Link } from 'react-router-dom';
 // import { ClassNames } from '@emotion/react';
 import { useTheme } from '@mui/material/styles';
 
-import { Sidebar } from '../index';
+import { Sidebar, Search } from '../index';
 import useStyles from './styles';
 
 const NavBar = () => {
@@ -46,7 +46,7 @@ const NavBar = () => {
           <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
             {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
           </IconButton>
-          {!isMobile && 'Search...'}
+          {!isMobile && <Search />}
           <div>
             {!isAuthenticated ? (
               <Button color="inherit" onClick={() => {}}>
@@ -69,7 +69,7 @@ const NavBar = () => {
               </Button>
             )}
           </div>
-          {isMobile && 'Search...'}
+          {isMobile && <Search />}
         </Toolbar>
       </AppBar>
       <div>
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
-const page = 1;
 
 export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
@@ -11,8 +10,12 @@ export const tmdbApi = createApi({
       query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
     }),
     getMovies: builder.query({
-      query: ({ genreIdOrCategoryName, page }) => {
+      query: ({ genreIdOrCategoryName, page, searchQuery }) => {
         // get movies by category
+        if (searchQuery) {
+          return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+        }
+
         if (
           genreIdOrCategoryName &&
           typeof genreIdOrCategoryName === 'string'
