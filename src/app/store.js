e7de7962c61742db73d7cc37594e2771785d0421
Yash Goswami <yashgoswami148@gmail.com>
2022-06-04 13:48:10 +0530

    authentication

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import { tmdbApi } from '../services/TMDB';
 
 import genreOrCategoryReducer from '../feature/currentGenreOrCategory';
+import userReducer from '../feature/auth';
 
 export default configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
     currentGenreOrCategory: genreOrCategoryReducer,
+    user: userReducer,
   },
 });
