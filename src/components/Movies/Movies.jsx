232de7903d347cb5e60f34aa4f7d1e5e92810a70
Yash Goswami <yashgoswami148@gmail.com>
2022-06-07 19:46:00 +0530

    featured movie

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   CircularProgress,
@@ -7,9 +7,7 @@ import {
 } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-import { Pagination } from '../index';
-import { genreOrCategory } from '../../feature/currentGenreOrCategory';
-import { MovieList } from '../index';
+import { Pagination, MovieList, FeaturedMovie } from '../index';
 import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
@@ -24,7 +22,7 @@ const Movies = () => {
   });
   const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
 
-  const numberOfMovies = lg ? 16 : 18;
+  const numberOfMovies = lg ? 17 : 19;
 
   if (isFetching) {
     return (
@@ -50,7 +48,8 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies={data} numberOfMovies={numberOfMovies} />
+      <FeaturedMovie movie={data.results[0]} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
       <Pagination
         currentPage={page}
         setPage={setPage}
